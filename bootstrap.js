a655555bb98394f4aeea5d2482711c6ad6201163
TenaciousBen <bhajduk@gmail.com>
2022-01-15 16:07:50 +0000

    added bootstrapper

---
+++ bootstrap.js
@@ -0,0 +1,24 @@
+const REPOSITORY_MASTER = "https://github.com/TenaciousBen/bitburner/blob/master/";
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	await fetch(ns, "hack.js");
+	await fetch(ns, "lib.js");
+	await fetch(ns, "main.js");
+	await fetch(ns, "path.js");
+	await fetch(ns, "sec.js");
+	await fetch(ns, "spider.js");
+	ns.exec("main.js", "home");
+}
+
+/** 
+ * @param {NS} ns
+ * @param {string} scriptNameWithExtension
+ * @returns Promise<void>
+ *  **/
+async function fetch(ns, scriptNameWithExtension) {
+	const url = REPOSITORY_MASTER + scriptNameWithExtension;
+	const successful = await ns.wget(url, scriptNameWithExtension, "home");
+	if (!successful) throw `Could not download script ${scriptNameWithExtension}`;
+	ns.tprint(`${scriptNameWithExtension} downloaded to home`);
+}
